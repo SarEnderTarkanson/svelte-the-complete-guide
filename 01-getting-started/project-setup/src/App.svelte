b4bd9abee8e0bdc7d9bb0125308d1bf840a201c4
Alparslan Tarkan <atarkan@hotmail.com>
2025-11-11 15:25:39 +0100

    more reactivity

--- 01-getting-started/project-setup/src/App.svelte
+++ 01-getting-started/project-setup/src/App.svelte
@@ -1,24 +1,31 @@
 <script>
-	let name="Alpy";
-	let age=39;
+	let name = "Alpy";
+	let age = 39;
 
-	$:uppercaseName = name.toUpperCase();
+	$: uppercaseName = name.toUpperCase();
 
-	function incrementAge(){
-		age +=1;
+	$: console.log(name);
+
+	$: if (name === "Alparslan") {
+		age = 19;
 	}
 
-	function changeName(){
+	function incrementAge() {
+		age += 1;
+	}
+
+	function changeName() {
 		name = "Alparslan";
+		age = 19;
 	}
 </script>
+
+<h1>Hello {uppercaseName} and my age is {age}!</h1>
+<button on:click={incrementAge}>change age</button>
+<button on:click={changeName}>change name</button>
 
 <style>
 	h1 {
 		color: purple;
 	}
 </style>
-
-<h1>Hello {uppercaseName} and my age is {age}!</h1>
-<button on:click="{incrementAge}">change age</button>
-<button on:click="{changeName}">change name</button>
