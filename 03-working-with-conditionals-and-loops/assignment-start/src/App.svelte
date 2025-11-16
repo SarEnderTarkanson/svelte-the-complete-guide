9735e4fb0f95a03c381022461ce99ec3e906f99e
Alparslan Tarkan <atarkan@hotmail.com>
2025-11-16 03:08:36 +0100

    closer look at list keys

--- 03-working-with-conditionals-and-loops/assignment-start/src/App.svelte
+++ 03-working-with-conditionals-and-loops/assignment-start/src/App.svelte
@@ -37,7 +37,7 @@
   {/if}
 
   <ul>
-    {#each passwords as pw, i}
+    {#each passwords as pw, i(pw)}
       <!-- svelte-ignore a11y_click_events_have_key_events -->
       <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
       <li on:click={removePassword.bind(this, i)}>{pw}</li>
